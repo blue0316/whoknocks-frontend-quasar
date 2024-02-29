ee2fae811eb7aee88c5f4145ae23990188e7ce12
blue0316 <greatblueknight@gmail.com>
2024-02-29 01:17:44 +0100

    Refactor IndexPage.vue to use ReadyPlayerMe component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <ReadyPlayerMe />
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import ReadyPlayerMe from 'src/components/ReadyPlayerMe.vue';
 
-export default defineComponent({
-  name: 'IndexPage'
-})
 </script>
