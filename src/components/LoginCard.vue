86e872a7cafc9b2ea88622b9e94ace4f188b2441
blue0316 <greatblueknight@gmail.com>
2024-02-29 10:21:00 +0100

    Add LoginCard component

---
+++ src/components/LoginCard.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+import { useLoginViewStore } from 'src/stores/login-view'
+
+const loginView = useLoginViewStore()
+const username = ref('')
+const password = ref('')
+
+</script>
+<template>
+  <q-dialog
+    v-model="loginView.getLoginView"
+    persistent
+  >
+    <q-card>
+      <q-form
+        class="q-pa-md"
+      >
+        <q-input
+          class="q-mb-md"
+          v-model="username"
+          label="Username"
+          outlined
+        />
+        <q-input
+          class="q-mb-md"
+          v-model="password"
+          label="Password"
+          type="password"
+          outlined
+        />
+        <q-btn
+          class="q-mb-md q-mr-md"
+          color="primary"
+          label="Login"
+          @click="login"
+        />
+        <q-btn
+          class="q-mb-md"
+          color="primary"
+          label="Cancel"
+          @click="loginView.closeLogin()"
+        />
+      </q-form>
+    </q-card>
+  </q-dialog>
+</template>
