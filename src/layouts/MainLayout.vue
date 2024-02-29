10e7295f4265e6534b337051ecf9497b70894540
blue0316 <greatblueknight@gmail.com>
2024-02-29 10:21:15 +0100

    Refactor MainLayout.vue component and add LoginCard component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,83 @@
+<script setup>
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+import LoginCard from 'src/components/LoginCard.vue'
+import { useLoginViewStore } from 'src/stores/login-view'
+
+const loginView = useLoginViewStore()
+const leftDrawerOpen = ref(false)
+
+const linksList = [
+  {
+    title: 'Avatar',
+    caption: 'Customize your avatar',
+    icon: 'face',
+    link: '/'
+  },
+  {
+    title: 'Lands',
+    caption: 'Buy a piece of land',
+    icon: 'landscape',
+    link: '/land'
+  }
+]
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+<q-layout view="lHh Lpr lFf">
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggleLeftDrawer"
+      />
+      <q-toolbar-title>
+
+      </q-toolbar-title>
+
+      <div>
         <q-btn
           flat
           dense
           round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+          icon="login"
+          aria-label="login"
+          @click="loginView.openLogin()"
         />
+      </div>
+    </q-toolbar>
+  </q-header>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+  <q-drawer
+    v-model="leftDrawerOpen"
+    show-if-above
+    bordered
+  >
+    <q-list>
+      <q-item-label
+        header
+      >
+        Essential Links
+      </q-item-label>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+      <EssentialLink
+        v-for="link in linksList"
+        :key="link.title"
+        v-bind="link"
+      />
+    </q-list>
+  </q-drawer>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+  <q-page-container>
+    <router-view />
+  </q-page-container>
+</q-layout>
+<LoginCard v-bind="open = loginOpen" />
 </template>
-
-<script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
-</script>
