58bfd9be259698ba772c997f18f9f0029eace567
blue0316 <greatblueknight@gmail.com>
2024-02-29 10:20:54 +0100

    Refactor EssentialLink component to use Vue 3 composition API

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,9 +1,41 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+// CONSTANTS //
+
+const router = useRouter()
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+
+  caption: {
+    type: String,
+    default: ''
+  },
+
+  link: {
+    type: String,
+    default: '#'
+  },
+
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+
+// FUNCTIONS //
+
+function routeToLink() {
+  router.push(props.link)
+}
+</script>
 <template>
   <q-item
     clickable
-    tag="a"
-    target="_blank"
-    :href="link"
+    @click="routeToLink"
   >
     <q-item-section
       v-if="icon"
@@ -19,31 +51,3 @@
   </q-item>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    icon: {
-      type: String,
-      default: ''
-    }
-  }
-})
-</script>
