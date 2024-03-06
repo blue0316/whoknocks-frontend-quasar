c83355c35d3c49400ad88485ac97a1b768b38eee
blue0316 <greatblueknight@gmail.com>
2024-03-06 09:42:10 +0100

    Add RegisterPage.vue with RegisterForm component

---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import RegisterForm from 'src/components/RegisterForm.vue';
+</script>
+<template>
+  <q-page class="flex flex-center background ">
+    <RegisterForm />
+  </q-page>
+</template>
+<style scoped>
+.background {
+  background: linear-gradient(to bottom left, #401454, #1e6a74);
+}
+</style>
