0373105d3ef31d0f3600fa46f37f987cd39ac836
blue0316 <greatblueknight@gmail.com>
2024-03-06 09:41:27 +0100

    Add login with MetaMask functionality and update email input validation

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -1,10 +1,31 @@
 <script setup>
 import { ref } from 'vue'
 import { useLoginViewStore } from 'src/stores/login-view'
+import { ethers } from 'ethers'
+
+// CONSTANTS //
 
 const loginView = useLoginViewStore()
-const username = ref('')
+
+const mail = ref('')
 const password = ref('')
+
+// FUNCTIONS //
+
+async function loginWithMetaMask() {
+  if (window.ethereum) {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress()
+      console.log('address', address)
+    } catch (error) {
+      console.error('error', error)
+    }
+  }
+}
+
 
 </script>
 <template>
@@ -14,30 +35,49 @@ const password = ref('')
   >
     <q-card>
       <q-form
+        @submit="login"
         class="q-pa-md"
       >
         <q-input
           class="q-mb-md"
-          v-model="username"
-          label="Username"
+          type="mail"
+          v-model="mail"
+          label="mail"
           outlined
+          lazy-rules
+          :rules="[ (val, rules) => rules.mail(val) || 'Invalid mail address']"
         />
         <q-input
-          class="q-mb-md"
           v-model="password"
           label="Password"
           type="password"
           outlined
         />
+        <q-card-section>
+          <router-link
+            to="/register"
+            @click="loginView.closeLogin()"
+          >
+            Register new account
+          </router-link>
+        </q-card-section>
         <q-btn
-          class="q-mb-md q-mr-md"
-          color="primary"
-          label="Login"
-          @click="login"
+          class="q-mr-md"
+          color="secondary"
+          text-color="black"
+          label="Login with metamask"
+          @click="loginWithMetaMask"
         />
         <q-btn
-          class="q-mb-md"
-          color="primary"
+          class="q-mr-md"
+          color="secondary"
+          text-color="black"
+          label="Login"
+          type="submit"
+        />
+        <q-btn
+          color="grey"
+          text-color="black"
           label="Cancel"
           @click="loginView.closeLogin()"
         />
