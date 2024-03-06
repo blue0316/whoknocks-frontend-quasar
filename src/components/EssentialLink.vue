5b67f4579066f21a4a5f23fdf8e8c5d028ac2c8e
blue0316 <greatblueknight@gmail.com>
2024-03-06 09:41:18 +0100

    Update EssentialLink component styling

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -41,12 +41,12 @@ function routeToLink() {
       v-if="icon"
       avatar
     >
-      <q-icon :name="icon" />
+      <q-icon color="white" :name="icon" />
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>{{ caption }}</q-item-label>
+      <q-item-label style="color: white;">{{ title }}</q-item-label>
+      <q-item-label style="color: white" caption>{{ caption }}</q-item-label>
     </q-item-section>
   </q-item>
 </template>
