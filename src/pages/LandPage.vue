2b6ba0f609bec0491832c94eb1d7b7d60d2696e0
blue0316 <greatblueknight@gmail.com>
2024-03-17 23:53:57 +0100

    Update LandPage.vue to use squares instead of hexagons for land display

--- src/pages/LandPage.vue
+++ src/pages/LandPage.vue
@@ -1,11 +1,8 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="main q-pa-md">
-      <div class="container">
-        <div class="hexagon" v-for="n in 100" :key="n" :id="'hexagon-' + n" @click="handleClick(n)"></div>
-      </div>
-    </div>
-    <q-dialog
+  <div class="map q-ma-md">
+    <div class="square" v-for="n in 10600" :key="n" :id="'square-' + n" @click="handleClick(n)" :class="{ 'clicked': clickedSquare === n }"></div>
+  </div>
+  <q-dialog
       v-model="openLandDialog"
       persistent
       transition-show="slide-up"
@@ -14,7 +11,7 @@
       <q-card>
         <q-card-section>
           <q-card-title>
-            Buy this Land: ID - {{ clickedHexagon }}
+            Buy this Land: ID - {{ clickedSquare }}
           </q-card-title>
         </q-card-section>
         <q-card-section>
@@ -31,56 +28,38 @@
         </q-card-section>
       </q-card>
     </q-dialog>
-  </q-page>
 </template>
+
 <script setup>
 import { ref } from 'vue';
-const clickedHexagon = ref(0);
+
+const clickedSquare = ref(null);
 const openLandDialog = ref(false);
 
 const handleClick = (id) => {
-  console.log('Hexagon ' + id + ' clicked!');
-  clickedHexagon.value = id;
+  clickedSquare.value = id;
   openLandDialog.value = true;
 };
 
 const closeLandDialog = () => {
   openLandDialog.value = false;
-  clickedHexagon.value = 0;
+  clickedSquare.value = 0;
 };
 </script>
+
 <style scoped>
-.main {
-  display:flex;
-  --s: 100px;  /* size  */
-  --m: 4px;    /* margin */
-  --f: calc(1.732 * var(--s) + 4 * var(--m)  - 1px);
+.map {
+  display: flex;
+  flex-wrap: wrap;
 }
 
-.container {
-  font-size: 0; /*disable white space between inline block element */
+.square {
+  width: 12px;
+  height: 12px;
+  border: 0.5px solid #000;
 }
 
-.container div {
-  width: var(--s);
-  margin: var(--m);
-  height: calc(var(--s)*1.1547); 
-  display: inline-block;
-  font-size:initial;
-  clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
-  background: #9C27B0;
-  margin-bottom: calc(var(--m) - var(--s)*0.2885); 
-}
-.container div:nth-child(odd) {
-  background: #00e8d9;
-}
-.container::before {
-  content: "";
-  width: calc(var(--s)/2 + var(--m));
-  float: left;
-  height: 120%;
-  shape-outside: repeating-linear-gradient(     
-                   #0000 0 calc(var(--f) - 3px),      
-                   #000  0 var(--f));
+.clicked {
+  background-color: gray;
 }
 </style>
