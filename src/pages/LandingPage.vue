5f870b65b74546ededfcd470ae879cfb474911d5
blue0316 <greatblueknight@gmail.com>
2024-03-28 14:23:16 +0000

    Add LandingPage component and route

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HomeFirst from 'src/components/HomeFirst.vue';
+import HomeSecond from 'src/components/HomeSecond.vue';
+import HomeThird from 'src/components/HomeThird.vue'; 
+import HomeAvatar from 'src/components/HomeAvatar.vue';
+import HomeParthners from 'src/components/HomeParthners.vue';
+import HomeRoadMap from 'src/components/HomeRoadMap.vue';
+import HomeFooters from 'src/components/HomeFooters.vue';
+</script>
+<template>
+    <q-page>
+        <HomeFirst />
+        <HomeSecond />
+        <HomeThird />
+        <HomeAvatar />
+        <HomeParthners />
+        <HomeRoadMap />
+        <HomeFooters />
+    </q-page>
+</template>
