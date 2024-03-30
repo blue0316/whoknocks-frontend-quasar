1a401960fcc0ea4dedb335e406631473c316c769
blue0316 <greatblueknight@gmail.com>
2024-03-30 16:34:43 +0000

    Update HomeFirst.vue to add LocomotiveScroll functionality

--- src/components/HomeFirst.vue
+++ src/components/HomeFirst.vue
@@ -1,10 +1,46 @@
 <script setup>
+import { onMounted, onUnmounted } from 'vue';
+import LocomotiveScroll from 'locomotive-scroll';
+
+onMounted(() => {
+    scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true,
+        multiplier: .2
+        // Add more options as needed
+    });
+});
+
+onUnmounted(() => {
+    scroll.destroy();
+});
 </script>
 <template>
-    <div 
-        class="background"
-    >
-    </div>
+<div class="background">
+  <div class="row justify-center align-center" style="height: 100vh; flex-direction: column;">
+    <section data-scroll-section>
+
+		<div class="col-12">
+			<p style="font-size: 100px; color: transparent; -webkit-text-stroke: 3px white; text-align: center; margin: 0">
+				EMPOWER YOUR LIFE
+			</p>
+		</div>
+    </section>
+
+    <!-- section with different speed multiplier -->
+    <section data-scroll-section data-scroll-speed="0.5">
+  
+      <div class="row justify-center">
+        <p style="font-size:50px; color: white; margin: 0">
+          in a new
+        </p>
+        <p style="font-size:80px; color: white; margin: 0">
+          REALITY
+        </p>
+      </div>
+    </section> 
+  </div>
+</div>
 </template>
 <style scoped>
     /* Set bg to src/assets/backgrounds/Bar_Under_Tower_2_1.png */
