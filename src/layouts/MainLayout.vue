73ebae5bdf8727c79ff0bf27746fbf4fd408db93
blue0316 <greatblueknight@gmail.com>
2024-03-30 16:35:17 +0000

    Update MainLayout.vue to add right drawer functionality and adjust header styles

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,7 +5,7 @@ import LoginCard from 'src/components/LoginCard.vue'
 import { useLoginViewStore } from 'src/stores/login-view'
 
 const loginView = useLoginViewStore()
-const leftDrawerOpen = ref(false)
+const rightDrawerOpen = ref(false)
 
 const linksList = [
   {
@@ -23,63 +23,178 @@ const linksList = [
 ]
 
 function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+  rightDrawerOpen.value = !rightDrawerOpen.value
 }
 </script>
 <template>
 <q-layout view="lHh Lpr lFf">
   
-  
-  <q-header 
-  style="background-color: #15161f;"
-  elevated>
-    <q-toolbar>
+      <!-- <q-btn
+      class="q-my-md"
+      color="secondary"
+      label="Login"
+      text-color="black"
+      aria-label="login"
+      @click="loginView.openLogin()"
+    /> -->
+<div 
+	class="row justify-between q-mt-md q-mb-md q-ml-md q-mr-md"
+	style="position: fixed; top: 0; right: 0; left:0; z-index: 9999; display: flex; align-items: center;">
+	<div
+	  class="col-4"
+	>
+		<img 
+			class=""
+			src="src/assets/metarise_logo_extended.png" 
+			alt="logo"
+		/>
+	</div>
 
-      <q-toolbar-title>
+	<div
+		class="col-4"
+		style="display: flex; justify-content: center; align-items: center;"
+	>
+	<img 	
+		class="	"
+		src="src/assets/metarise_logo.svg" 
+		alt="Logo" 
+	/>
+	</div>
+	<div
+		class="col-4"
+		style="display: flex; justify-content: flex-end; align-items: center;"
+	>
+		<q-btn
+			size="20px"
+			class=""
+			color="primary"
+			text-color="white"
+			dense
+			round
+			:icon="rightDrawerOpen ? 'close' : 'menu'"
+			aria-label="Menu"
+			@click="toggleLeftDrawer"
+		/>
+	</div>
+</div>
 
-      </q-toolbar-title>
 
-      <div>
-        <q-btn
-          class="q-my-md"
-          color="secondary"
-          label="Login"
-          text-color="black"
-          aria-label="login"
-          @click="loginView.openLogin()"
-        />
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-      </div>
-    </q-toolbar>
-  </q-header>
 
-  <q-drawer
-    side="right"
-    style="background-color: #21222a;"
-    v-model="leftDrawerOpen"
-    overlay
+<q-drawer
+  side="right"
+  style="background-color: #86ECE9;"
+  v-model="rightDrawerOpen"
+  overlay
+	width=450
+>
+  <q-list 
+  class="q-mt-md"
   >
-    <q-list 
-      class="q-mt-md"
-    >
-      <EssentialLink
-        v-for="link in linksList"
-        :key="link.title"
-        v-bind="link"
-      />
-    </q-list>
-  </q-drawer>
+   <div
+	 	style="height: 55px;"
+   >
 
-  <q-page-container>
-    <router-view />
-  </q-page-container>
+   </div>
+   <!-- Login -->
+	 <div
+	 	class="q-mr-xl"
+	 >
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 Login
+		 </q-item-section>
+		 </q-item>
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 World Map
+		 </q-item-section>
+		 </q-item>
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 F.Q.A
+		 </q-item-section>
+		 </q-item>
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 Support
+		 </q-item-section>
+		 </q-item>
+
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+			 class="q-mt-xl"
+		 >
+		 <q-item-section class="text-h2" style="text-align: right; color: white">
+			 METAARISE
+		 </q-item-section>
+		 </q-item>
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 CHAINLIGHT CITY
+		 </q-item-section>
+		 </q-item>  
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 LIVE, PLAY, EARN
+		 </q-item-section>
+		 </q-item>
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 BE YOUR SELF
+		 </q-item-section>
+		 </q-item>
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 COMMUNITY 
+		 </q-item-section>
+		 </q-item>
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 ROADMAP
+		 </q-item-section>
+		 </q-item> 
+		 <q-item
+			 clickable
+			 @click="loginView.openLogin"
+		 >
+		 <q-item-section class="text-h4" style="text-align: right; color: white">
+			 CONTACT
+		 </q-item-section>
+		 </q-item> 
+	 </div>
+  </q-list>
+</q-drawer>
+<q-page-container>
+  <router-view />
+</q-page-container>
 </q-layout>
 <LoginCard />
 </template>
