c683299aacb8a370e668a530bfeefa7c152f84c5
blue0316 <greatblueknight@gmail.com>
2024-03-30 16:34:56 +0000

    Update HomeSecond.vue to add new chapter section

--- src/components/HomeSecond.vue
+++ src/components/HomeSecond.vue
@@ -2,8 +2,41 @@
 </script>
 <template>
     <div 
-        class="background"
+        class="background row justify-center align-center flex items-center"
+
     >
+
+    <div class="col-6 offset-6 text-center q-pr-lg">
+        
+        <div style="font-size: 70px; line-height: 1; color: white">
+            A NEW CHAPTER  FOR
+            THE HUMAN RACE
+        </div>
+    
+        <div class="q-mt-md" style="font-size: 15px; color: white">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.
+        </div>
+        <q-btn
+          class="q-mt-md"
+          style="border-radius: 120px;"
+          color="secondary"
+          text-color="white"
+          label="Join Chainlight City"
+        />
+        <div style="font-size: 70px; font-weight: bold; color: white" >
+            5
+        </div>
+        <div style="font-size: 23px; font-weight: bold; color: white;  background: linear-gradient(to right, #86ECE9, #967BF8);">
+            POINTS OF INTERESTS
+        </div>
+        <div style="font-size: 70px; font-weight: bold; color: white" >
+            10.600
+        </div>
+        <div style="font-size: 23px; font-weight: bold; color: white;  background: linear-gradient(to right, #86ECE9, #967BF8);">
+            LANDS
+        </div> 
+    </div>
+
     </div>
 </template>
 <style scoped>
