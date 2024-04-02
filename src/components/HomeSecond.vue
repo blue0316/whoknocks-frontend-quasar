4a35dc22ef5ad34644670535f53cf1bfc7899a78
blue0316 <greatblueknight@gmail.com>
2024-04-02 12:38:17 +0000

    Update HomeSecond.vue to add new chapter section and adjust styles

--- src/components/HomeSecond.vue
+++ src/components/HomeSecond.vue
@@ -6,7 +6,37 @@
 
     >
 
-    <div class="col-6 offset-6 text-center q-pr-lg">
+    <div class="col-6 offset-6 text-center q-pa-lg gt-sm">
+        
+        <div style="font-size: 70px; line-height: 1; color: white">
+            A NEW CHAPTER  FOR
+            THE HUMAN RACE
+        </div>
+    
+        <div class="q-mt-md" style="font-size: 15px; color: white">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.
+        </div>
+        <q-btn
+          class="q-mt-md"
+          style="border-radius: 120px;"
+          color="secondary"
+          text-color="white"
+          label="Join Chainlight City"
+        />
+        <div style="font-size: 70px; font-weight: bold; color: white" >
+            5
+        </div>
+        <div style="font-size: 23px; font-weight: bold; color: white;  background: linear-gradient(to right, #86ECE9, #967BF8);">
+            POINTS OF INTERESTS
+        </div>
+        <div style="font-size: 70px; font-weight: bold; color: white" >
+            10.600
+        </div>
+        <div style="font-size: 23px; font-weight: bold; color: white;  background: linear-gradient(to right, #86ECE9, #967BF8);">
+            LANDS
+        </div> 
+    </div>
+    <div class="col-12 text-center q-pa-lg lt-md">
         
         <div style="font-size: 70px; line-height: 1; color: white">
             A NEW CHAPTER  FOR
@@ -46,7 +76,7 @@
         background-repeat: no-repeat;
         background-size: cover;
         background-position: center;
-        height: 100vh; /* Adjust as needed */
+        min-height: 100vh; /* Adjust as needed */
         width: 100%; /* Adjust as needed */
     }
 </style>
