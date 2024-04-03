36d1933b98a018dd34e101c6e281dd397b558487
blue0316 <greatblueknight@gmail.com>
2024-04-03 14:38:01 +0000

    Update LandingPage.vue to add LocomotiveScroll functionality

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,25 +1,25 @@
 <template>
     <q-page>
         <div data-scroll-container>
-            <section data-scroll-section>
+            <section data-scroll-section id="home">
                 <HomeFirst />
             </section>
-            <section data-scroll-section>
+            <section data-scroll-section id="chainlight-city">
                 <HomeSecond />
             </section>
-            <section data-scroll-section>
+            <section data-scroll-section id="live-play-earn">
                 <HomeThird />
             </section>
-            <section data-scroll-section>
+            <section data-scroll-section id="be-your-self">
                 <HomeAvatar />
             </section>
-            <section data-scroll-section>
+            <section data-scroll-section id="community">
                 <HomeParthners />
             </section>
-            <section data-scroll-section>
+            <section data-scroll-section id="roadmap">
                 <HomeRoadMap />
             </section>
-            <section data-scroll-section>
+            <section data-scroll-section id="contact">
                 <HomeFooters />
             </section>
         </div>
@@ -27,7 +27,8 @@
 </template>
 
 <script setup>
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted, ref, watch } from 'vue';
+import { useScrollStore } from 'src/stores/scroll-store';
 import LocomotiveScroll from 'locomotive-scroll';
 import HomeFirst from 'src/components/HomeFirst.vue';
 import HomeSecond from 'src/components/HomeSecond.vue';
@@ -39,16 +40,25 @@ import HomeFooters from 'src/components/HomeFooters.vue';
 
 let scroll;
 
+const scrollStore = useScrollStore();
+
+
+watch(scrollStore, (value) => {
+    console.log(value.getSection);
+    const target = document.querySelector('#' + value.getSection);
+
+    scroll.scrollTo(target);
+
+    scrollStore.setSection(null);
+});
+
 onMounted(() => {
+    
     scroll = new LocomotiveScroll({
         el: document.querySelector('[data-scroll-container]'),
         smooth: true,
         multiplier: .2
         // Add more options as needed
     });
-});
-
-onUnmounted(() => {
-    scroll.destroy();
 });
 </script>
