61bfb2ad0970f444a1047ae5361b9b59f551845d
blue0316 <greatblueknight@gmail.com>
2024-04-03 14:36:46 +0000

    Update HomeFooters.vue to add follow us section and adjust styles

--- src/components/HomeFooters.vue
+++ src/components/HomeFooters.vue
@@ -1,9 +1,27 @@
 <script setup>
+    import { ref } from 'vue'
+    const email = ref('')
 </script>
 <template>
     <div 
-        class="background"
+        class="background column justify-center align-center"
     >
+    <div class="row justify-center flex flex-center">
+        <div class="text col-12 text-center gt-sm">
+            FOLLOW US
+        </div>
+        <div class="text-small col-12 text-center lt-md">
+            FOLLOW US
+        </div>
+        <q-input class="col-6 q-ma-md" v-model="email" filled type="email" bg-color="white" square/>
+        <q-btn 
+            class="col-6 col-md-2 q-ma-md"
+            style="border-radius: 120px;"
+            color="primary"
+            text-color="white"
+            label="submit"
+        />
+    </div>
     </div>
 </template>
 <style scoped>
@@ -13,7 +31,19 @@
         background-repeat: no-repeat;
         background-size: cover;
         background-position: center;
-        height: 462px; /* Adjust as needed */
+        min-height: 462px; /* Adjust as needed */
         width: 100%; /* Adjust as needed */
+    }
+    .text {
+        font-size: 130px;
+        line-height: 1;
+        color: white;
+        font-weight: bolder;
+    }
+    .text-small {
+        font-size: 50px;
+        line-height: 1;
+        color: white;
+        font-weight: bolder;
     }
 </style>
