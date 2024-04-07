9d73509898d4042fcb9b689b2508cb341053651c
blue0316 <greatblueknight@gmail.com>
2024-04-07 20:44:51 +0000

    Update HomeAvatar.vue to improve UI and add new content

--- src/components/HomeAvatar.vue
+++ src/components/HomeAvatar.vue
@@ -4,7 +4,7 @@
 <div class="background row justify-center align-center relative">
     <div class="overlay-content absolute text-center">
         <img src="src/assets/ready_player_me_logo.png" alt="logo">
-        <div class="q-mt-md" style="font-size: 18px; color: white; max-width: 469px;">
+        <div class="q-mt-md" style="font-size: 18px; max-width: 469px;">
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </div>
         <q-btn
@@ -15,11 +15,14 @@
           label="Create Your Avatar"
         />
     </div>
-	<div class="text-center col-12" style="font-size: 50px; line-height: 1; color: white; font-weight: bolder; margin-top: 100px;">
-		A <span style="color: #86ECE9;">LIVE</span> <br> CHAPTER FOR
+	<div class="text-center col-12" style="font-size: 50px; line-height: 1; font-weight: 800; margin-top: 100px;">
+		BECOME A <span style="color: #86ECE9;">RISER</span>
 	</div>
 	<div class="q-mt-md text-center" style="font-size: 18px; color: white; max-width: 700px;">
-		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
+        In Metarise you are free to truly be whoever you want. 
+        Each riser in Chainlight City is perfectly recognizable and different from the others: 
+        you can choose to wear a jacket for a gala date or a tracksuit 
+        for a cricket match by quickly going through your wardrobe in the villa.
 	</div>
 <img src="src/assets/avatar_home.png" alt="" style="height: 100%; object-fit: contain; margin-top: 30px"/>
 </div>
