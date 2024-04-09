ddf197e8c53603253dfd5ec25186ca3efa021fbe
blue0316 <greatblueknight@gmail.com>
2024-04-09 09:44:55 +0000

    Update countdown timer and text styles in HomeToken.vue

--- src/components/HomeToken.vue
+++ src/components/HomeToken.vue
@@ -4,14 +4,14 @@ import { ref, onMounted } from 'vue';
 const countdownTime = ref('00 : 00 : 00');
 
 onMounted(() => {
-  const targetTime = new Date("Apr 10, 2024 11:00:00").getTime();
+  const targetTime = new Date("May 24, 2024 11:00:00").getTime();
   setInterval(() => {
     const now = new Date().getTime();
     const distance = targetTime - now;
-    const hours = Math.floor(distance / (1000 * 60 * 60)).toString().padStart(2, '0');
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2, '0');
-    countdownTime.value = `${hours} : ${minutes} : ${seconds}`;
+    countdownTime.value = `${days} : ${hours} : ${minutes}`;
   }, 1000);
 });
 
@@ -22,22 +22,30 @@ function openNewPage() {
 <template>
 <div class="background">
     <div style="padding-top: 100px;">
-        <div class="text-center title-text col-12 gt-xs">
+        <div class="text-center text-h-desktop-extrabold col-12 gt-sm">
             EMPOWERING <span style="color: #86ECE9;"> POSSIBILITIES </span>
         </div>
-        <div class="text-center title-text-small col-12 lt-sm">
-            UNLOCK LIMITLESS <span style="color: #86ECE9;"> <br> POSSIBILITIES</span>
+        <div class="text-center q-px-md text-h-mobile-extrabold col-12 lt-md" style="font-size: 25px;">
+            EMPOWERING <span style="color: #86ECE9;"> <br> POSSIBILITIES</span>
         </div>
         <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: 40px;">
-  <div class="text-center text" style="text-align: center;">
+  <div class="text-center text-p-desktop gt-sm" style="text-align: center; max-width: 800px;">
+    Metarise is fueled by its official cryptocurrency $RIS, extending its utility far beyond transactions, captivating users in a web3 in a new economy of endless possibilities.
+  </div>
+  <div class="text-center q-px-md text-p-mobile lt-md" style="text-align: center; max-width: 400px;">
     Metarise is fueled by its official cryptocurrency $RIS, extending its utility far beyond transactions, captivating users in a web3 in a new economy of endless possibilities.
   </div>
   <div class="text-center" style="text-align: center; font-size: 20px; font-weight: bold; margin-top: 40px">
     PRIVATE SALE LIVE...
   </div>
   <q-linear-progress query color="primary" class="q-mt-md q-mx-md" style="max-width: 400px;"/>
-  <div class="bg" style="padding: 60px; display: flex; justify-content: center; align-items: center; height: 100%; margin-top: 40px;">
-    <div style="background: linear-gradient(to right bottom, #28272F, #000000); font-size: 64px; border-radius: 20px; border: 1px solid rgba(255, 255, 255, 0.3); padding-left: 20px; padding-right: 20px;">
+  <div class="bg gt-xs" style="padding: 60px; display: flex; justify-content: center; align-items: center; height: 100%; margin-top: 40px;">
+    <div style="background: linear-gradient(to right bottom, #28272F, #000000); font-size: 64px; border-radius: 20px; border: 1px solid rgba(255, 255, 255, 0.3); padding-left: 20px; padding-right: 20px; padding-bottom: 12px;">
+      {{ countdownTime }}
+    </div>
+  </div>
+  <div class="bg lt-sm" style="padding: 45px; display: flex; justify-content: center; align-items: center; height: 100%; margin-top: 40px;">
+    <div style="background: linear-gradient(to right bottom, #28272F, #000000); font-size: 40px; border-radius: 20px; border: 1px solid rgba(255, 255, 255, 0.3); padding-left: 20px; padding-right: 20px; padding-bottom: 12px">
       {{ countdownTime }}
     </div>
   </div>
@@ -55,12 +63,20 @@ function openNewPage() {
 </div>
 <div class="container">
     <div class="marquee marquee1">
+      <span style="font-size: 250px; font-weight: 800;">$RIS $RIS $RIS</span>
+    </div>
+    <!-- <div class="marquee marquee2">
+    <span style="font-size: 250px; font-weight: 800;">$RIS $RIS $RIS</span>
+    </div> -->
+</div>
+<!-- <div class="container gt-sm">
+    <div class="marquee marquee1">
     <span style="font-size: 250px; font-weight: 800;">$RIS $RIS</span>
     </div>
     <div class="marquee marquee2">
     <span style="font-size: 250px; font-weight: 800;">$RIS $RIS</span>
     </div>
-</div>
+</div> -->
 </div>
 </template>
 <style scoped>
@@ -98,7 +114,7 @@ function openNewPage() {
 }
 .container {
   position: relative;
-  z-index: 9999;
+  z-index: 1;
   width: 100%;
   height: 300px
 }
